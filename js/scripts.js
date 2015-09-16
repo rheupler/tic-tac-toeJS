94fbbb96a375da44a6022a10edda55a0bc90a58d
travis crampton & Ryan Heupler <student@epicodus.com>
2015-09-16 14:42:28 -0700

    adds working winning message

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,97 @@
+function Game() {
+  this.xTurn = [];
+  this.oTurn = [];
+}
+
 var turn = "X";
 
-function startGame()  {
-  alert( turn + " gets to start.");
-};
+var game = new Game();
 
-var switchPlayer = function() {
+
+$(function() {
+  $(".square").click(function(event){
+    event.preventDefault();
+
+    var selectedSquare = $(this).attr('id');
+    console.log(selectedSquare);
+
+    if (turn === "O") {
+    game.xTurn.push(selectedSquare);
+  } else {
+    game.oTurn.push(selectedSquare);
+  }
+
+  if ((game.oTurn.indexOf('1') > -1) && (game.oTurn.indexOf('2') > -1) && (game.oTurn.indexOf('3') > -1)) {
+    alert("X WINS");
+  } else if ((game.oTurn.indexOf('4') > -1) && (game.oTurn.indexOf('5') > -1) && (game.oTurn.indexOf('6') > -1)) {
+    alert("O WINS")
+  } else if ((game.oTurn.indexOf('7') > -1) && (game.oTurn.indexOf('8') > -1) && (game.oTurn.indexOf('9') > -1)) {
+    alert("O WINS")
+  } else if ((game.oTurn.indexOf('1') > -1) && (game.oTurn.indexOf('4') > -1) && (game.oTurn.indexOf('7') > -1)) {
+    alert("O WINS")
+  } else if ((game.oTurn.indexOf('2') > -1) && (game.oTurn.indexOf('5') > -1) && (game.oTurn.indexOf('8') > -1)) {
+    alert("O WINS")
+  } else if ((game.oTurn.indexOf('3') > -1) && (game.oTurn.indexOf('6') > -1) && (game.oTurn.indexOf('9') > -1)) {
+    alert("O WINS")
+  } else if ((game.oTurn.indexOf('1') > -1) && (game.oTurn.indexOf('5') > -1) && (game.oTurn.indexOf('9') > -1)) {
+    alert("O WINS")
+  } else if ((game.oTurn.indexOf('3') > -1) && (game.oTurn.indexOf('5') > -1) && (game.oTurn.indexOf('7') > -1)) {
+    alert("O WINS")
+  } else if ((game.xTurn.indexOf('1') > -1) && (game.xTurn.indexOf('2') > -1) && (game.xTurn.indexOf('3') > -1)) {
+    alert("X WINS");
+  } else if ((game.xTurn.indexOf('4') > -1) && (game.xTurn.indexOf('5') > -1) && (game.xTurn.indexOf('6') > -1)) {
+    alert("X WINS")
+  } else if ((game.xTurn.indexOf('7') > -1) && (game.xTurn.indexOf('8') > -1) && (game.xTurn.indexOf('9') > -1)) {
+    alert("X WINS")
+  } else if ((game.xTurn.indexOf('1') > -1) && (game.xTurn.indexOf('4') > -1) && (game.xTurn.indexOf('7') > -1)) {
+    alert("X WINS")
+  } else if ((game.xTurn.indexOf('2') > -1) && (game.xTurn.indexOf('5') > -1) && (game.xTurn.indexOf('8') > -1)) {
+    alert("X WINS")
+  } else if ((game.xTurn.indexOf('3') > -1) && (game.xTurn.indexOf('6') > -1) && (game.xTurn.indexOf('9') > -1)) {
+    alert("X WINS")
+  } else if ((game.xTurn.indexOf('1') > -1) && (game.xTurn.indexOf('5') > -1) && (game.xTurn.indexOf('9') > -1)) {
+    alert("X WINS")
+  } else if ((game.xTurn.indexOf('3') > -1) && (game.xTurn.indexOf('5') > -1) && (game.xTurn.indexOf('7') > -1)) {
+    alert("X WINS")
+  }
+  });
+
+});
+
+
+
+//
+//
+//
+//
+//
+// function startGame()  {
+//   alert(turn + " gets to start.");
+// };
+//
+function switchPlayer() {
   if (turn === "X") {
     turn = "O";
   } else {
     turn = "X"
   }
 }
-
-
+//
+// function winner(a, b, c, move) {
+//
+//   if (checkRow(1, 2, 3, move)) {
+//     alert("dafa");
+//   }
+// }
+//
+//
+//
 function nextMove(square) {
   square.innerText = turn;
   switchPlayer();
+
 }
+//
+// $(function() {
+//   $("h2#turns").append(turn + "'s turn");
+// });
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,9 @@ describe('player', function() {
   it('starts the game and shows whos turn it is', function() {
     expect(turn).to.eq("X")
   });
+
+  it('will alternate players after a player goes', function() {
+    var turn = "X";
+    expect(turn.switchPlayer()).to.eq("O");
+  });
 });
